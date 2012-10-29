5dd1881dc2728070e3600a78b21219e3b075491c
fabiosammy <fabiosammy@gmail.com>
2012-10-29 12:28:05 -0200

    Update AODV TCC text

--- doc/TCC/images-source/build.sh
+++ doc/TCC/images-source/build.sh
@@ -4,4 +4,6 @@ neato -Teps experimento1.dot -o ../images/experimento1.eps
 neato -Teps experimento2.dot -o ../images/experimento2.eps
 neato -Teps dsdvOperation.dot -o ../images/dsdvOperation.eps
 dot -Teps aodvOperation.dot -o ../images/aodvOperation.eps
+dot -Teps aodvRREQ.dot -o ../images/aodvRREQ.eps
+dot -Teps aodvRREP.dot -o ../images/aodvRREP.eps
 
